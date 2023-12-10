973ea70970179f809b617b2d042ad9b5d6594704
Your Name <khon17801@gmail.com>
2023-12-10 09:28:39 +0700

    code navbar voi footer

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,7 +1,58 @@
-const Footer = () => {
-    return(
-        <div>Footer</div>
-    )
-}
+import Link from 'next/link';
+import Container from '../Container';
+import FooterList from './FooterList';
+import { MdFacebook } from 'react-icons/md';
 
-export  default Footer;
+const Footer = () => {
+  return (
+    <footer className="bg-slate-700 text-slate-200 text-sm mt-16">
+      <Container>
+        <div className="flex flex-col md:flex-row justify-between pt-16 pb-8">
+          <FooterList>
+            <h3 className="text-base font-bold mb-2">Model categories</h3>
+            <Link href="#">Decoration</Link>
+            <Link href="#">Childroom</Link>
+            <Link href="#">Funiture</Link>
+            <Link href="#">Plants</Link>
+            <Link href="#">Textures</Link>
+            <Link href="#">Kitchen</Link>
+            <Link href="#">Bathroom</Link>
+            <Link href="#">Other Model</Link>
+            <Link href="#">Materials</Link>
+            <Link href="#">Scripts</Link>
+            <Link href="#">Technology</Link>
+          </FooterList>
+          <FooterList>
+            <h3 className="text-base font-bold mb-2">Customer Service</h3>
+            <Link href="#">Contract Us</Link>
+            <Link href="#">Visual Training</Link>
+            <Link href="#">Privacy Policy</Link>
+            <Link href="#">Support</Link>
+          </FooterList>
+          <div className="w-full md:w-1/3 mb-6 md:mb-0">
+            <h3 className="text-base font-bold mb-2">About Us</h3>
+            <p className="mb-2">
+              Tầm Nhìn: Chúng tôi hướng đến việc trở thành nguồn cung cấp hàng
+              đầu các mô hình 3D sáng tạo và chất lượng cao, đáp ứng mọi nhu cầu
+              của người dùng trong ngành [ngành công nghiệp]. Sứ Mệnh: Sứ mệnh
+              của chúng tôi là đem đến giải pháp mô hình 3D tiên tiến, phong phú
+              và đa dạng, hỗ trợ cho các dự án sáng tạo và tương tác trong thế
+              giới kỹ thuật số.
+            </p>
+            <p>&copy;{new Date().getFullYear()} 3D Online.All right reserved</p>
+          </div>
+          <FooterList>
+            <h3 className="text-base font-bold mb-2">Follow Us</h3>
+            <div className="flex gap-2">
+              <Link href="#">
+                <MdFacebook size={24} />
+              </Link>
+            </div>
+          </FooterList>
+        </div>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
--- app/components/nav/NavBar.tsx
+++ app/components/nav/NavBar.tsx
@@ -1,24 +1,29 @@
-import Container from "../Container";
-import Link from "next/link";
+import Container from '../Container';
+import Link from 'next/link';
+import { Redressed } from 'next/font/google';
+const redressed = Redressed({ subsets: ['latin'], weight: ['400'] });
 const NavBar = () => {
-    return (
-        <div className="sticky top-0 w-full bg-slate-200 z-30 shadow-sm">  
-            <div className="py-4 border-b-[1px]">
-                <Container>
-                    <div>
-                        <Link href='/'>3D Online</Link>
-                            <div>Search</div>
-                            <div>
-                            <div>
-                                CartCount
-                            </div>
-                            <div>UserMenu</div>
-                        </div>
-                    </div>
-                </Container>
+  return (
+    <div className="sticky top-0 w-full bg-slate-200 z-30 shadow-sm">
+      <div className="py-4 border-b-[1px]">
+        <Container>
+          <div className="flex items-center justify-between gap-3 md-gap-0 ">
+            <Link
+              href="/"
+              className={'${redressed.className} font-bold text-2xl'}
+            >
+              3D Online
+            </Link>
+            <div className="hidden md:block">Search</div>
+            <div className="flex items-center gap-8 md:gap-12">
+              <div>CartCount</div>
+              <div>UserMenu</div>
             </div>
-        </div>
-    )
-}
+          </div>
+        </Container>
+      </div>
+    </div>
+  );
+};
 
-export  default NavBar;
+export default NavBar;
